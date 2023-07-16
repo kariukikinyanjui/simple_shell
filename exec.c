db1ab63032f2659e23bedbc10a0ee7eaf0ef211d
Tai Keith <keithsteve.ks@hotmail.com>
2023-07-15 17:28:21 -0700

    [Feature]: This file handles the PATH

---
+++ exec.c
@@ -0,0 +1,54 @@
+#include "shell.h"
+
+void file_path(char *command)
+{
+	char *path;
+	char *dir;
+	char full_path[BUFF_SIZE];
+	int status;
+
+	path = getenv("PATH");
+
+	if (path == NULL)
+	{
+		printf("PATH environment variable not found\n");
+	}
+
+	dir = strtok(path, ":");
+	while (dir != NULL)
+	{
+		strncpy(full_path, dir, BUFF_SIZE - 1);
+		full_path[BUFF_SIZE - 1] = '\0';
+		strcat(full_path, "/");
+		strcat(full_path, command);
+
+		if (access(full_path, F_OK) == 0)
+		{
+			pid_t pid = fork();
+
+			if (pid < 0)
+			{
+				perror("fork");
+				exit(EXIT_FAILURE);
+			}
+			else if (pid == 0)
+			{
+				char *argv[] = {full_path, NULL};
+				execve(full_path, argv, environ);
+				perror("execve");
+				exit(EXIT_FAILURE);
+			}
+			else {
+				waitpid(pid, &status, 0);
+				if (WIFEXITED(status) && WEXITSTATUS(status) == 0)
+				{
+					return;
+				}
+			}
+		}
+
+		dir = strtok(NULL, ":");
+	}
+
+	printf("Command not found\n");
+}
