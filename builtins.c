fd8d86ff2ce4dbd8393b012f9c3a1681b9821271
kariukikinyanjui <michaelkariuki7@gmail.com>
2023-07-16 14:59:26 +0000

    Implemented the exit built-in command

--- builtins.c
+++ builtins.c
@@ -4,7 +4,20 @@
  * exit_func - implementation of the exit built-in command
  * Return: exit status
  */
-void exit_func()
+void exit_func(void)
 {
 	exit(EXIT_SUCCESS);
+}
+/**
+ * env_func - implementation the env built-in command
+ */
+void env_func(void)
+{
+	char **env = environ;
+
+	while (*env != NULL)
+	{
+		printf("%s\n", *env);
+		env++;
+	}
 }
